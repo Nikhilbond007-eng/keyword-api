0f3d4622c314475acac6fe4d92a6f9f388fc73ae
Nikhil Raj <nikhilashu936@gmail.com>
2026-02-26 13:35:07 +0530

    final clean app structure

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from google.ads.googleads.client import GoogleAdsClient
-
-app = Flask(__name__)
-
+from flask import Flask, request, jsonify
 import os
 from google.ads.googleads.client import GoogleAdsClient
 
+# Create Flask app
+app = Flask(__name__)
+
+# Load credentials from Render environment variables
 config = {
     "developer_token": os.environ.get("DEVELOPER_TOKEN"),
     "client_id": os.environ.get("CLIENT_ID"),
@@ -15,27 +16,31 @@ config = {
 
 client = GoogleAdsClient.load_from_dict(config)
 
+
 @app.route("/keywords")
 def get_keywords():
     keyword = request.args.get("keyword")
 
-    customer_id ="916-455-2447"
+    customer_id = "9164552447"  # your ads customer id (no dashes if error later)
 
     ga_service = client.get_service("GoogleAdsService")
 
-    query = f"""
-        SELECT
-          campaign.id
+    query = """
+        SELECT campaign.id
         FROM campaign
         LIMIT 1
     """
 
-    response = ga_service.search(customer_id=customer_id, query=query)
+    response = ga_service.search(
+        customer_id=customer_id,
+        query=query
+    )
 
     return jsonify({
         "status": "API working ✅",
         "keyword": keyword
     })
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
